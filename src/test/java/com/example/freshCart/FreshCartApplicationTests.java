295ce7f8bfcf23da999d37dddabf219665dad255
jieun-dev1 <jkdev1324@gmail.com>
2022-08-25 12:59:39 +0900

    initial project setting

---
+++ src/test/java/com/example/freshCart/FreshCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freshCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
