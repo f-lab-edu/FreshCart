ac64d56c80cd9a7a5e3fafa4fa47faee9f1d16ed
jieun-dev1 <jkdev1324@gmail.com>
2022-09-01 17:16:27 +0900

    Feat: Request객체를 command 로 수정함

--- src/main/java/com/example/freshCart/infrastructure/UserInMemoryRepository.java
+++ src/main/java/com/example/freshCart/infrastructure/UserInMemoryRepository.java
@@ -1,9 +1,10 @@
 package com.example.freshCart.infrastructure;
 
+import com.example.freshCart.application.command.EmailDupCheckCommand;
+import com.example.freshCart.application.command.SignupCommand;
 import com.example.freshCart.domain.UserRepository;
 import com.example.freshCart.domain.User;
-import com.example.freshCart.presentation.request.EmailDupCheckRequest;
-import com.example.freshCart.presentation.request.SignupRequest;
+import com.example.freshCart.infrastructure.exception.EmailExistsException;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -18,32 +19,33 @@ public class UserInMemoryRepository implements UserRepository {
     private static Map<String, User> store = new ConcurrentHashMap<>();
     private static Long sequence = 0L;
 
-    @Override
-    public User save(SignupRequest request) {
+  @Override
+  public User save(SignupCommand request) {
         User user = new User(request.getEmail(), request.getPassword(), request.getName());
         user.setId(++sequence);
         store.put(user.getEmail(), user);
         return user;
     }
 
-    @Override
-    public Optional<User> findByLoginId(String email) {
+  // LoginId 와 일치하는 데이터가 있으면 꺼내올 것. FindFirst 를 붙여야, stream의 요소 중 조건에 맞는 첫 번째 결과를 반환함.
+  @Override
+  public Optional<User> findByLoginId(String email) {
         return findAll().stream().filter(u -> u.getEmail().equals(email)).findFirst();
     }
-
-    @Override
-    public List<User> findAll() {
+  /*
+  Collections를 리턴하기 때문에 ArrayList(Collections c)로 value를 직접 넣을 수 있다.
+  검색을 위해 User 객체 전부 (store의 값 전체)를 ArrayList에 넣는다.
+   */
+  @Override
+  public List<User> findAll() {
         return new ArrayList<>(store.values());
     }
 
-    @Override
-    public boolean findByEmail(EmailDupCheckRequest request) {
+  @Override
+  public void findByEmail(EmailDupCheckCommand request) {
         User user = store.getOrDefault(request.getEmail(), null);
-        if(user == null) {
-            return false;
+    if (user != null) {
+      throw new EmailExistsException("이미 존재하는 이메일입니다");
         }
-        else {
-            return true;
-        }
-    }
+  }
 }
