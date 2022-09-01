9194263a8d721e400bcff36dbfe4bfae816d653b
jieun-dev1 <jkdev1324@gmail.com>
2022-09-01 17:14:09 +0900

    Feat: 세션 추가 확인에 필요한 API 들 추가.

--- src/main/java/com/example/freshCart/presentation/UserController.java
+++ src/main/java/com/example/freshCart/presentation/UserController.java
@@ -1,58 +1,62 @@
 package com.example.freshCart.presentation;
 
-import com.example.freshCart.application.UserRegisterServiceV1;
-import com.example.freshCart.domain.AuthenticationService;
-import com.example.freshCart.domain.User;
-import com.example.freshCart.domain.UserRepository;
-import com.example.freshCart.infrastructure.exception.InvalidLoginTryException;
-import com.example.freshCart.presentation.request.EmailDupCheckRequest;
-import com.example.freshCart.presentation.request.LoginRequest;
-import com.example.freshCart.presentation.request.SignupRequest;
-import com.example.freshCart.util.SessionUtil;
+import com.example.freshCart.application.UserRegisterService;
+import com.example.freshCart.application.*;
+import com.example.freshCart.application.command.LoginCommand;
+import com.example.freshCart.application.command.SignupCommand;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*
-회원가입의 세부 로직이 바뀔 수 있음을 고려해 인터페이스에 의존하게 설계하자.
- */
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private UserRepository userRepository;
-    private UserRegisterServiceV1 userRegisterService;
-    private AuthenticationService authentication;
+  //  private AuthenticationService authentication;
+  private final SessionManager sessionManager;
+  private UserRegisterService userRegisterService;
 
-    public UserController(UserRepository userRepository,
-            UserRegisterServiceV1 userRegisterService) {
-        this.userRepository = userRepository;
-        this.userRegisterService = userRegisterService;
+  public UserController(UserRegisterService userRegisterService, SessionManager sessionManager) {
+    this.userRegisterService = userRegisterService;
+    this.sessionManager = sessionManager;
+  }
+
+  /*
+  회원 가입
+   */
+  @PostMapping("/signup")
+  public void signup(@RequestBody SignupCommand request) {
+    userRegisterService.register(request);
+  }
+
+  /*
+  exception 처리 필요.
+   */
+
+  @PostMapping("/login")
+  public void login(@RequestBody LoginCommand request, HttpServletResponse servletResponse) {
+    // 로그인 실패 시 예외처리가 되고, 성공 시 loginUser (ID, PW만 포함) 이 리턴됨.
+    LoginUser check = userRegisterService.signIn(request);
+    sessionManager.createSession(check, servletResponse);
+  }
+
+  @GetMapping("/home")
+  public String homeLogin(HttpServletRequest request) {
+    // 세션 매니저를 통해서 저장된 회원 정보를 조회
+    LoginUser user = sessionManager.getSession(request);
+    if (user == null) {
+      return "세션이 존재하지 않습니다";
+    } else {
+      return "환영합니다." + user.getEmail(); //
     }
+  }
 
-    /*
-    회원 가입
-     */
-    @PostMapping("/signup")
-    public void Signup(@RequestBody SignupRequest request){
-        userRegisterService.toRegister(request);
-    }
-
-    /*
-    exception 처리 필요.
-     */
-
-    @GetMapping("/login")
-    public void Login(@RequestBody LoginRequest request, HttpServletRequest httpRequest) {
-       User loginUser = userRegisterService.toSignIn(request);
-        if(loginUser == null) {
-            throw new InvalidLoginTryException( "아이디 또는 비밀번호가 일치하지 않습니다");
-      }
-        HttpSession session = httpRequest.getSession();
-        session.setAttribute(SessionUtil.LOGIN_USER, loginUser);
-    }
+  @PostMapping("/logout")
+  public void logout(HttpServletRequest request) {
+    sessionManager.expireSession(request);
+  }
 }
