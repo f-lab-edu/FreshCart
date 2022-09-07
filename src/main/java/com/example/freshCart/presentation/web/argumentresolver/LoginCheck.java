4fbf06efeb93a54e9a1d0fd705a958eabf75edab
jieun-dev1 <jkdev1324@gmail.com>
2022-09-07 15:29:17 +0900

    Feat: Path Pattern 대신 어노테이션을 적용
    -Path 변경 시 생길 수 있는 에러를 줄일 수 있음
    -로그인이 필요한 로직에 대해 컨트롤러 클래스에서 바로 확인 가능

---
+++ src/main/java/com/example/freshCart/presentation/web/argumentresolver/LoginCheck.java
@@ -0,0 +1,16 @@
+package com.example.freshcart.presentation.web.argumentresolver;
+
+/**
+ * 해당 경로에는 사용자 인증이 강제됨.
+ */
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginCheck {
+
+}
