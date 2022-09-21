a5a678e0facdf64e0b9fe7e9be2cad2bb6281cf6
jieun-dev1 <jkdev1324@gmail.com>
2022-09-21 19:43:43 +0900

    feature: enum 처리 -type-handlers 를 없애고, underscore를 camelcase로 직접 매핑이 아니라 resultMap에서 처리함. -기본 생성자를 추가하여 마이바티스에서 객체 바인딩을 제대로 실행할 수 있도록 함. -search와 enumCheck는 불필요하다고 판단하여 삭제

--- src/main/java/com/example/freshcart/domain/Role.java
+++ src/main/java/com/example/freshcart/domain/Role.java
@@ -5,25 +5,4 @@ package com.example.freshcart.domain;
  */
 public enum Role {
   USER, SELLER, ADMIN;
-
-  public static Role search(String columnName) {
-    for (Role element : Role.values()) {
-      if (element.name().equalsIgnoreCase(columnName)) {
-        return element;
-      }
-    }
-    return null;
-  }
-
-  public static Role enumCheck(String name) {
-    Role result = null;
-    for (Role role : values()) {
-      //일치한다면 반복문을 빠져나올 수 있다.
-      if (role.name().equalsIgnoreCase(name)) {
-        result = role;
-        break;
-      }
-    }
-    return result;
-  }
 }
