361c0918e96c0cb2f3bb9a26ff4a6b06986a0a81
jieun-dev1 <jkdev1324@gmail.com>
2022-09-26 09:53:57 +0900

    Chore: DROP TABLE IF EXISTS 삭제 (새롭게 테이블을 만들고 있는데 외래키 삭제 Restrict 조건이 걸리면서, drop table이 안되는 경우가 많아서 비효율적이라고 판단함)

--- src/main/resources/db/migration/V3__create_product.sql
+++ src/main/resources/db/migration/V3__create_product.sql
@@ -1,18 +1,17 @@
-drop table if exists product;
-
 create table product
 (
     id          int unsigned auto_increment,
-    name       varchar(80)  not null,
-    price    int not null,
-    status   varchar(20) not null,
+    name        varchar(80)  not null,
+    price       int          not null,
+    status      varchar(20)  not null,
     description varchar(255) not null,
-    single_type boolean not null,
-    created_at datetime DEFAULT CURRENT_TIMESTAMP,
-    updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    single_type boolean      not null,
+    created_at  datetime DEFAULT CURRENT_TIMESTAMP,
+    updated_at  datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     category_id int unsigned,
-    user_id int unsigned,
+    user_id     int unsigned,
     primary key (id),
-    FOREIGN KEY (category_id) references category(id) on DELETE RESTRICT on UPDATE RESTRICT,
-    FOREIGN KEY (user_id) references user(id) on DELETE RESTRICT on UPDATE RESTRICT
-) engine = InnoDB default character set = UTF8MB4;
+    FOREIGN KEY (category_id) references category (id) on DELETE RESTRICT on UPDATE RESTRICT,
+    FOREIGN KEY (user_id) references user (id) on DELETE RESTRICT on UPDATE RESTRICT
+) engine = InnoDB
+  default character set = UTF8MB4;
