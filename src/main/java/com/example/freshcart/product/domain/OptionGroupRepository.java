7d4ce04badb036777f1f2e94dabdf09ae0d167e5
jieun-dev1 <jkdev1324@gmail.com>
2022-10-08 00:58:17 +0900

    feat: checkstyle에 맞게 수정

--- src/main/java/com/example/freshcart/product/domain/OptionGroupRepository.java
+++ src/main/java/com/example/freshcart/product/domain/OptionGroupRepository.java
@@ -3,6 +3,8 @@ package com.example.freshcart.product.domain;
 import java.util.List;
 
 public interface OptionGroupRepository {
+
   OptionGroup save(OptionGroup optionGroup);
+
   List<OptionGroup> findByProductId(Long id);
 }
