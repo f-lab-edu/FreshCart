a9108286305ba2fdebc9576229cc66d028cd983b
jieun-dev1 <jkdev1324@gmail.com>
2022-12-23 23:32:01 +0900

    chore: @builder 적용

--- src/main/java/com/example/freshcart/user/application/command/LoginCommand.java
+++ src/main/java/com/example/freshcart/user/application/command/LoginCommand.java
@@ -1,25 +1,22 @@
 package com.example.freshcart.user.application.command;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class LoginCommand {
 
   private String email;
 
   private String password;
 
+  @Builder
   public LoginCommand(String email, String password) {
     this.email = email;
     this.password = password;
   }
 
   public LoginCommand() {
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public String getPassword() {
-    return password;
   }
 
 }
