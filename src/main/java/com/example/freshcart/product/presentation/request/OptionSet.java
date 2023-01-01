dbb8610fc92adeabf87048d7017229fced1116a7
jieun-dev1 <jkdev1324@gmail.com>
2023-01-01 11:40:59 +0900

    chore: builder 적용에 따른 noargs 추가

--- src/main/java/com/example/freshcart/product/presentation/request/OptionSet.java
+++ src/main/java/com/example/freshcart/product/presentation/request/OptionSet.java
@@ -1,13 +1,18 @@
 package com.example.freshcart.product.presentation.request;
 
+import com.example.freshcart.product.domain.Option;
+import com.example.freshcart.product.domain.OptionGroup;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.constraints.NotBlank;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 /**
  * OptionGroup 한 개당 하나 이상의 옵션을 갖는다 (ex. 중량 - 50g, 100g 등)
  */
 
+@NoArgsConstructor
 public class OptionSet {
 
   @NotBlank(message = "옵션 그룹을 최소 하나 입력하세요")
@@ -30,4 +35,18 @@ public class OptionSet {
   public List<OptionDetailRegister> getOptionDetailRegisterList() {
     return optionDetailRegisterList;
   }
+
+  public List<Option> toOptions(List<OptionDetailRegister> optionDetailRegisterList, OptionGroup optionGroup){
+    return optionDetailRegisterList
+        .stream()
+        .map(optionDetailRegister ->
+            Option.builder()
+                .name(optionDetailRegister.getName())
+                .price(optionDetailRegister.getPrice())
+                .optionGroupId(optionGroup.getId())
+                .sellerId(optionGroup.getSellerId())
+                .build())
+        .collect(Collectors.toList());
+  }
+
 }
